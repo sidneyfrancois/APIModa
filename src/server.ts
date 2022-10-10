c4fc8cb9c4847df8640721f9ea7d5c3979df937a
Sidney François <sidney.fvm.ps@outlook.com>
2022-10-10 14:21:30 -0300

    removendo cors

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,12 @@
 import "reflect-metadata";
 require("dotenv").config();
 import express from "express";
-import cors from "cors";
 import { routes } from "./routes";
 
 // Criação da conexão com o banco de dados através do TypeORM
 import "./database";
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 app.use(routes);
 
