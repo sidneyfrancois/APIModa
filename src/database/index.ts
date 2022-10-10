70d56231579a53b980741e38be121cc0e5f18abb
Sidney François <sidney.fvm.ps@outlook.com>
2022-10-10 14:57:21 -0300

    corrigindo extensao da build

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,7 +3,7 @@ import { createConnection } from "typeorm";
 require("dotenv").config();
 
 const rootDir = process.env.NODE_ENV === "development" ? "src" : "build";
-const extensionFile = process.env.NODE_ENV === "development" ? "ts" : "build";
+const extensionFile = process.env.NODE_ENV === "development" ? "ts" : "js";
 
 const config: any = {
   type: "postgres",
